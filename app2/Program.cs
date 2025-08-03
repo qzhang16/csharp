8a0a50f3effa84a2f8d12558a8d909a6c6ae2070
qzhang16 <qzhang16@ra.rockwell.com>
2025-08-03 18:27:47 +0800

    first commit

---
+++ app2/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using app3;
+
+Console.WriteLine("Hello, World!");
+    Class1 class1 = new Class1();
+    class1.Ace01();
---
+++ app3/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Data.Odbc;
+
+namespace app3
+{
+    public class Class1
+    {
+        public void Ace01()
+        {
+            Console.WriteLine("hello from Ace01 !");
+            string connectionString = "DSN=PLEX;Uid=qzhang16;";
+            using (OdbcConnection conn = new OdbcConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    Console.WriteLine("odbc connection is good !");
+                    // string sql01 = "select count(*) from part_v_part a";
+                    string sql01 = "select top 20 part_no from part_v_part a";
+                    using (OdbcCommand cmd = new OdbcCommand(sql01, conn))
+                    {
+                        // long count = (long)cmd.ExecuteScalar();
+                        // Console.WriteLine("part count: " + count);
+                        using (OdbcDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                string part_no = reader.GetString(0);
+                                Console.WriteLine("part no: " + part_no);
+                            }
+
+                        }
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("odbc error: " + e.Message);
+                }
+            }
+
+
+        }
+    }
+}
