bbabeea7209d785595dd90e2aa79c6889c6293bc
qzhang16 <qzhang16@ra.rockwell.com>
2025-08-04 21:52:16 +0800

    ad

--- app3/Class1.cs
+++ app3/Class1.cs
@@ -5,9 +5,14 @@ namespace app3
 {
     public class Class1
     {
+        public string Abc { get; set; }
+        public Class1( string a) {
+            this.Abc = a;
+        }
+
         public void Ace01()
         {
-            Console.WriteLine("hello from Ace01 !");
+            Console.WriteLine("hello from Ace01 !" + Abc);
             string connectionString = "DSN=PLEX;Uid=qzhang16;";
             using (OdbcConnection conn = new OdbcConnection(connectionString))
             {
@@ -40,6 +45,11 @@ namespace app3
             }
 
 
+        }
+
+        private string getAbc()
+        {
+            throw new NotImplementedException();
         }
     }
 }
